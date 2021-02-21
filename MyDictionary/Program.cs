84c957e230f5f24ff793d3ab7b0d3c1c6ac218eb
MyWorld <MyWorld@DESKTOP-HM25RS2>
2021-02-21 13:11:59 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Abide");
+            myDictionary.Add(2, "Bahar");
+            myDictionary.Add(3, "Bayram");
+            myDictionary.Add(4, "Burhan");
+
+            myDictionary.ToList();
+
+        }
+    }
+}
